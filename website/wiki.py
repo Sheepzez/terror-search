f351ede1ce49ccd4738b03d486de1cc054023c3f
Dillnot <stevensondylan13@googlemail.com>
2014-10-11 19:49:03 +0100

    brackthought on full text thanks to cap itl

--- website/wiki.py
+++ website/wiki.py
@@ -6,17 +6,13 @@ import string
 
 def all_text(inp):
     current_page = wikipedia.page(wikipedia.search(inp, results = 1))# search finds what it thinks is most relivent not the exact right match
-    test = (current_page.content).split("\n")
-    print(len(test))
-    remo = []
-    for x in test:
-        if x =="":
-            test.remove(x)
-    print (len(test))
-    for x in test:
-        print (x)
-        print("__________________________________________________________")
-        
+    
+    one = re.search("[^=]==[^=](.*)[^=]==[^=]", current_page.content)
+    print (one)
+    #for x in one:
+        #print(x)
+    
+            
 
 def most_freq(inp):
     current_page = wikipedia.page(wikipedia.search(inp, results = 1))
