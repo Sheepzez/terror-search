d317cf6ede2ebf12efdf2a4bdc84b6e584208bc4
Dillnot <stevensondylan13@googlemail.com>
2014-10-11 17:15:54 +0100

    key words function done

--- website/wiki.py
+++ website/wiki.py
@@ -1,11 +1,30 @@
 import wikipedia
+import nltk
 import re
+from nltk.corpus import stopwords
+import string
 
 def all_text(inp):
-    current_page = wikipedia.page(wikipedia.search(inp))# search finds what it thinks is most relivent not the exact right match
-    section_heads = re.findall("== (.*) ==", current_page.content)
-    output = {}
-    for head in section_heads:
-        output[head]= current_page.section(head)
-    return output
+    current_page = wikipedia.page(wikipedia.search(inp, results = 1))# search finds what it thinks is most relivent not the exact right match
+    test = (current_page.content).split("\n")
+    print(len(test))
+    remo = []
+    for x in test:
+        if x =="":
+            test.remove(x)
+    print (len(test))
+    for x in test:
+        print (x)
+        print("__________________________________________________________")
+        
 
+def most_freq(inp):
+    current_page = wikipedia.page(wikipedia.search(inp, results = 1))
+    list_words= ((current_page.content.translate(string.punctuation)).lower()).split()
+    reavent_words = [w for w in list_words if not w in stopwords.words('english')]
+    fdisk = nltk.FreqDist(reavent_words)
+    return fdisk.most_common(50)
+
+
+
+all_text("glasgow")
