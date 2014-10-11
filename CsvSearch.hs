573f1bdc6e0306f9920d3193f15728b2e2521171
Dr-Lord <Lord.Fletcher@outlook.com>
2014-10-11 14:14:52 +0100

    Working

--- CsvSearch.hs
+++ CsvSearch.hs
@@ -5,20 +5,20 @@ import Text.CSV (parseCSV)
 import Data.List (isInfixOf)
 
 main = do
-    -- args <- getArgs
-    -- let query = read $ head args :: [(String, String)]
+    args <- getArgs
+    let query = read $ head args :: [(String, String)]
     -- print query
     contents <- readFile "BIGBOOK.csv"
     let (ts:ls) = lines contents
-    let titles = toNice $ parseCSV "BIGBOOK.csv" ts
+    let titles = head . toNice $ parseCSV "BIGBOOK.csv" ts
     let eithErrCsv = map (parseCSV "BIGBOOK.csv") $ ls
     let lazyLines = map toNice eithErrCsv
     
     -- return . toJSON $ search titles query lazyLines
     -- return $ search titles query lazyLines
     -- return . toJSON $ search titles [("Data", "Kokang")] lazyLines
-    print . take 2 $ lazyLines
-    -- print . take 2 $ search titles query lazyLines
+    -- print . take 2 $ lazyLines
+    print . take 2 $ search titles query lazyLines
     -- return $ take 2 lazyLines
     -- return $ toJSON titles
 
@@ -27,14 +27,14 @@ toNice x = case x of
     Left  err  -> []
     Right cs -> cs :: [[String]]
 
-search :: [String] -> [(String, String)] -> [[String]] -> [[String]]
+search :: [String] -> [(String, String)] -> [[[String]]] -> [[String]]
 search titles keyWords doc = foldr step [] doc
-    where step :: [String] -> [[String]] -> [[String]]
+    where step :: [[String]] -> [[String]] -> [[String]]
           step entry acc = case matches titles keyWords entry of
-                    True  -> entry:acc
+                    True  -> (head entry):acc
                     False -> acc
 
-matches ts kw entr = foldr step False $ zip ts entr
+matches ts kw entr = foldr step False $ zip ts (head entr)
     where step (t,e) acc = case lookup t kw of
             Just value -> if value `isInfixOf` e
                             then True
